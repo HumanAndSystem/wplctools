b704750a0738bea7b5f7209980f38347ccdfcea4
LSY <grizlupo@daum.net>
2022-04-21 10:39:06 +0900

    change setup

--- setup.py
+++ setup.py
@@ -1,14 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
-# packages = find_packages()
-# print(packages)
-setup(
-    name="wplctools",
-    version="0.1",
-    package_dir={"": "src"},
-    packages=[
-        "wplctools",
-    ],
-    test_suite="tests",
-)
+setup()
